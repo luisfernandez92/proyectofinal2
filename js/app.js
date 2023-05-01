df14c16cdb943778ddb0676178d5b76b5286ce50
Luis Fernandez <luisfdez1992@gmail.com>
2023-05-01 15:39:28 -0600

    Avance 3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let ingresos = [new Ingreso('Salario', 20000), new Ingreso('Venta auto', 50000)];
+const egresos = [new Egreso('Renta', 4000), new Egreso('Ropa', 800)];
+
 const cargarCabecero = () => {
     let presupuesto = totalIngresos() - totalEgresos();
     let porcentajeEgreso = totalEgresos() / totalIngresos();
@@ -12,10 +15,10 @@ const cargarCabecero = () => {
 const totalIngresos = () => {
     let totalIngreso = 0;
     let ingreso = 0;
-    let ingresos = [9000, 400]
+    // let ingresos = [9000, 400]
 
     for (ingreso of ingresos) {
-        totalIngreso = totalIngreso + ingreso;
+        totalIngreso = totalIngreso + ingreso['_valor'];
     }
 
     return totalIngreso;
@@ -24,10 +27,10 @@ const totalIngresos = () => {
 const totalEgresos = () => {
     let totalEgreso = 0;
     let egreso = 0;
-    let egresos = [900, 400]
+    // let egresos = [900, 400]
 
     for (egreso of egresos) {
-        totalEgreso += egreso;
+        totalEgreso += egreso['_valor'];
     }
 
     return totalEgreso;
